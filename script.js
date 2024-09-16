7f6ea9cd7e3f8d9b32e9d4406e44cfa1e95495c2
Sonjabrunell <sonjabrunell@live.se>
2024-09-16 15:23:24 +0200

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-// Här kan du lägga till eventuella JavaScript-funktioner eller interaktivitet.
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Profil sidan är nu laddad och klar.');
-});
