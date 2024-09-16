06e0954f447671495afd35044339cf6bd7304121
Sonjabrunell <sonjabrunell@live.se>
2024-09-16 15:03:53 +0200

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,4 @@
+// Här kan du lägga till eventuella JavaScript-funktioner eller interaktivitet.
+document.addEventListener('DOMContentLoaded', () => {
+    console.log('Profil sidan är nu laddad och klar.');
+});
